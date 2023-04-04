f98ee2f0235462b7eb8acca5061c77ad63371930
yash koladiya <123717379+yashkoladiya1@users.noreply.github.com>
2023-04-04 15:28:58 +0530

    validation of form

--- main.js
+++ main.js
@@ -1,38 +1,42 @@
 var arr = [];
 
 function onHandleSubmit() {
-  let x = document.getElementById("firstName").value;
-  let y = document.getElementById("middleName").value;
-  let z = document.getElementById("lastName").value;
-  let c = document.getElementById('cities').value;
-  if (x == "") {
-    document.getElementById('firname').innerHTML="<p style='color:red'>please enter first name</p>"
-    return false;
-  }else if (y == "") {
-    document.getElementById('midname').innerHTML="<p style='color:red'>please enter middle name</p>"
-    return false;
-  } else if (z == "") {
-    document.getElementById('lasname').innerHTML="<p style='color:red'>please enter last name</p>"
-    return false; 
-  }else if(c == "Cities"){
-    document.getElementById('selectcity').innerHTML="<p style='color:red'>please select city</p>"
-    return false; 
-  }else{
+  let f = document.getElementById("firstName");
+  let m = document.getElementById("middleName");
+  let l = document.getElementById("lastName");
+  let c = document.getElementById("cities");
+  if (f.value == "" && m.value == "" && l.value == "") {
+    var buttons = document.getElementsByClassName('error');
+
+    for(var i=0; i< buttons.length; i++){
+        buttons[i].innerHTML = "<p style='color:red'>please fill the field</p>";
+    }
+  }else if(f.value ==""){
+    document.getElementById("fi").innerHTML="<p style='color:red'>please enter firstName</p>"
+  }else if(m.value ==""){
+    document.getElementById("mi").innerHTML="<p style='color:red'>please enter middleName</p>"
+  }else if(l.value ==""){
+    document.getElementById("la").innerHTML="<p style='color:red'>please enter lastName</p>"
+  }else if(c.value == "city"){
+    document.getElementById("selectcity").innerHTML="<p style='color:red'>please select city</p>"
+  } else {
     var formData = {};
     formData["firstName"] = document.getElementById("firstName").value;
     formData["middleName"] = document.getElementById("middleName").value;
     formData["lastName"] = document.getElementById("lastName").value;
-    formData["gender"]= document.querySelector('input[name="gender"]:checked').value;
-    formData['cities']=document.getElementById('cities').value;
-    var checks = document.getElementsByClassName('hobby');
-    var str='';
-    for(i=0; i<4;i++){
-      if(checks[i].checked === true){
-        str += checks[i].value +  " ";
+    formData["gender"] = document.querySelector(
+      'input[name="gender"]:checked'
+    ).value;
+    formData["cities"] = document.getElementById("cities").value;
+    var checks = document.getElementsByClassName("hobby");
+    var str = "";
+    for (i = 0; i < 4; i++) {
+      if (checks[i].checked === true) {
+        str += checks[i].value + " ";
       }
     }
-    formData['hobby']= document.querySelectorAll('hobby').value = str;
-    console.log(formData)
+    formData["hobby"] = document.querySelectorAll("hobby").value = str;
+    console.log(formData);
     var index = document.getElementById("inde").value;
     if (index == "" || index == undefined) {
       arr.push(formData);
@@ -57,7 +61,7 @@ function insertNewRecord(arr) {
     k += "<td>" + data.lastName + "</td>";
     k += "<td>" + data.gender + "</td>";
     k += "<td>" + data.cities + "</td>";
-    k += "<td>" + data.hobby + "</td>"
+    k += "<td>" + data.hobby + "</td>";
     k += `<td><button class='btn btn-success' onClick=onEdit(${i})>Edit</button>
         <button class='btn btn-danger' onClick=onDelete(${i})>Delete</button></td>`;
     k += "</tr>";
@@ -71,15 +75,18 @@ function resetForm() {
   document.getElementById("lastName").value = "";
   document.getElementById("male1").checked = true;
   document.getElementById("female2").checked = false;
-  document.getElementById('cities').value = "Cities";
-  document.getElementById('firname').innerHTML = "";
-  document.getElementById('midname').innerHTML = "";
-  document.getElementById('lasname').innerHTML = "";
-  document.getElementById('selectcity').innerHTML ="";
-  document.getElementById('play').checked = false;
-  document.getElementById('movie').checked = false;
-  document.getElementById('gym').checked = false;
-  document.getElementById('travel').checked = false;
+  document.getElementById("cities").value = "Cities";
+  document.getElementById("selectcity").innerHTML = "";
+  document.getElementById("play").checked = false;
+  document.getElementById("movie").checked = false;
+  document.getElementById("gym").checked = false;
+  document.getElementById("travel").checked = true;
+  document.getElementById("inde").value = "";
+  var buttons = document.getElementsByClassName('error');
+
+  for(var i=0; i< buttons.length; i++){
+      buttons[i].innerHTML = "";
+  }
 }
 
 function onEdit(i) {
@@ -88,30 +95,31 @@ function onEdit(i) {
   document.getElementById("middleName").value = val.middleName;
   document.getElementById("lastName").value = val.lastName;
   document.getElementById("inde").value = i;
-  var gvalue = document.querySelectorAll('input[name="gender"]').value = val.gender;
-  var cvalue = document.getElementsByName('hobby').value = val.hobby;
-  console.log(cvalue)
+  var gvalue = (document.querySelectorAll('input[name="gender"]').value =
+    val.gender);
+  var cvalue = (document.getElementsByName("hobby").value = val.hobby);
+  console.log(cvalue);
   var travell = cvalue.includes("travelling");
   var moviee = cvalue.includes("movie");
   var plays = cvalue.includes("playing");
   var gymm = cvalue.includes("gym");
-  var playing = document.getElementById('play');
-  var movieing = document.getElementById('movie');
-  var gyming = document.getElementById('gym');
-  var travelling = document.getElementById('travel');
-  var m = document.getElementById('male1');
-  var f = document.getElementById('female2');
-  if(gvalue == 'male'){
+  var playing = document.getElementById("play");
+  var movieing = document.getElementById("movie");
+  var gyming = document.getElementById("gym");
+  var travelling = document.getElementById("travel");
+  var m = document.getElementById("male1");
+  var f = document.getElementById("female2");
+  if (gvalue == "male") {
     m.checked = true;
-  }else{
-   f.checked = true;
+  } else {
+    f.checked = true;
   }
 
-  if(travell == true){
+  if (travell == true) {
     travelling.checked = true;
   }
 
-  if(moviee == true){
+  if (moviee == true) {
     movieing.checked = true;
   }
 
@@ -122,7 +130,7 @@ function onEdit(i) {
   if(gymm == true){
     gyming.checked = true;
   }
-  document.getElementById('cities').value = val.cities;
+  document.getElementById("cities").value = val.cities;
   // console.log(val);
   document.getElementById("btn-user").innerText = "Update User";
 }
@@ -131,6 +139,5 @@ function onDelete(i) {
   arr.splice(i, 1);
   insertNewRecord(arr);
 }
-
 
 
